052f8315ec62f756b3dc98b2df27cce9f366528c
Jess Robins <j.robins@thelevelup.com>
2018-07-26 22:39:08 -0400

    can't believe this actually works

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,7 @@
-import { FirebaseService } from 'tt-module'
+import firebase from 'react-native-firebase'
 import store from './store'
 
-import {
-  VUE_APP_FIREBASE_API_KEY,
-  VUE_APP_FIREBASE_AUTH_DOMAIN,
-  VUE_APP_FIREBASE_DATABASE_URL,
-  VUE_APP_FIREBASE_PROJECT_ID,
-  VUE_APP_FIREBASE_STORAGE_BUCKET,
-  VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-} from 'react-native-dotenv'
-
-const firebase = new FirebaseService({
-  apiKey: VUE_APP_FIREBASE_API_KEY,
-  authDomain: VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-})
+firebase.firestore().settings({ timestampsInSnapshots: true })
 
 export function authPromise() {
   return new Promise(async (resolve, reject) => {
